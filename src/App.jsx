8e333c561967789a62d6a5db33c818f65c1f07af
Nora Petro Torres <128987028+Norapetro@users.noreply.github.com>
2023-10-14 15:36:58 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header/Header";
 import PokemonList from "./components/PokemonList/PokemonList";
+import mockData from "./assets/mockData.json";
 import "./App.css";
 
 function App() {
+  console.log(mockData)
   return (
     <>
       <div className="container">
